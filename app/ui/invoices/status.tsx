8fdd407aff102a6b8f126d79841f87ce651acb68
guilhaof <luisphelipe1000@gmail.com>
2023-11-11 13:32:43 -0300

    initial commit version 0.1.0

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -7,7 +7,7 @@ export default function InvoiceStatus({ status }: { status: string }) {
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-yellow-500 text-white': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
         },
       )}
